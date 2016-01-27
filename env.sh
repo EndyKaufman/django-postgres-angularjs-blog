c3d648ae6953d0d1a8164caece0f49f42487864d
EndyKaufman <admin@site15.ru>
2016-01-27 10:46:23 +0500

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export DATABASE_URL=postgres://database
+export ON_PRODUCTION=0
+export USE_AMAZONE=0
+export S3_BUCKET_NAME=django-postgres-angularjs-blog
+export USE_FIXTURE=1
+export AWS_SECRET_ACCESS_KEY=secret_key
+export AWS_ACCESS_KEY_ID=access_key
