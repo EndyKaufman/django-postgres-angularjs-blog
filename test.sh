acf5bd6acfd8419150aec4a544f1d3eaa5bdc447
EndyKaufman <admin@site15.ru>
2016-03-04 19:04:38 +0500

    change front

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 source venv/bin/activate
 export $(cat .env)
-export DISPLAY_=:10
+export DISPLAY=:10
 cd front
-gulp test --host http://127.0.0.1:5000 --isvagrant_ true --file_ project/admin.js --debug_ true
+gulp test --host http://127.0.0.1:5000 --isvagrant true --file_ project/admin.js --debug true --display 10
 cd ../
