275c8f77d02492fcb4a7eff1c1c7f43e0d64b0f0
EndyKaufman <admin@site15.ru>
2016-03-06 13:41:04 +0500

    add travis.yml

--- run.sh
+++
@@ -1,7 +0,0 @@
-source venv/bin/activate
-export $(cat .env)
-cd front
-gulp build --env development
-cd ../
-python manage.py collectstatic --noinput
-gunicorn project.wsgi -b 0.0.0.0:5000
--- test.sh
+++
@@ -1,6 +0,0 @@
-source venv/bin/activate
-export $(cat .env)
-export DISPLAY=:10
-cd front
-gulp test --host http://127.0.0.1:5000 --isvagrant true --file_ project/admin.js --debug true --display 10
-cd ../
