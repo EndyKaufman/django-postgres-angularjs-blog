ceb73e9d158673e31469748118807c9943910525
EndyKaufman <admin@site15.ru>
2016-05-04 14:52:38 +0500

    change browser on protractor

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,6 +10,7 @@ sudo apt-get install -y default-jre
 sudo apt-get install -y default-jdk
 sudo apt-get install -y openjdk-7-jre
 sudo apt-get install -y openjdk-7-jdk
+sudo apt-get install firefox -y
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get install xvfb -y
