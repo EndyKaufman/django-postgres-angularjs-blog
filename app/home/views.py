6b412791586288a9804305a07fae1d5e1a042825
EndyKaufman <admin@site15.ru>
2016-05-15 01:47:47 +0500

    update method name

--- app/home/views.py
+++ app/home/views.py
@@ -48,7 +48,7 @@ def index(request):
 
 def robots_txt(request):
     """Robots.txt page"""
-    properties_list = properties_helpers.getListOfNames(
+    properties_list = properties_resource.get_list_of_names(
         ['ROBOT_TXT'])
     return render(request, 'home/templates/%s/robots.txt' % settings.THEME, {'properties_list': properties_list})
 
