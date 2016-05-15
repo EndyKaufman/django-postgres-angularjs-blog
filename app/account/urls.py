85afd13c8b92694190f825eb9d3726cf854d375a
EndyKaufman <admin@site15.ru>
2016-05-15 22:42:16 +0500

    update urls

--- app/account/urls.py
+++ app/account/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^login', views.login),
-    url(r'^profile', views.profile),
-    url(r'^reg', views.reg),
-    url(r'^recovery', views.recovery),
-    url(r'^reset', views.reset),
-    url(r'^user_app', views.user_app)
+    url(r'^/login', views.login),
+    url(r'^/profile', views.profile),
+    url(r'^/reg', views.reg),
+    url(r'^/recovery', views.recovery),
+    url(r'^/reset', views.reset),
+    url(r'^/user_app', views.user_app)
 ]
--- app/project/urls.py
+++ app/project/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^(?P<project_name>[-\w]+)', views.get_item_by_name),
+    url(r'^/(?P<project_name>[-\w]+)', views.get_item_by_name),
     url(r'^$', views.get_list)
 ]
